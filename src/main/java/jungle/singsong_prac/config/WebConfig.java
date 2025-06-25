88795708b0c5e19b28cdb82d49cac99f89f2553e
hwaseongcityboy <eun4005@gmail.com>
2025-06-25 11:27:37 +0900

    CICD Test

---
+++ src/main/java/jungle/singsong_prac/config/WebConfig.java
@@ -0,0 +1,15 @@
+package jungle.singsong_prac.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Next.js dev 서버
+                .allowedMethods("*");
+    }
+}
